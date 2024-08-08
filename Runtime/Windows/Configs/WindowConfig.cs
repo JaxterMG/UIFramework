30587c3f7d232f749490deec7b7e209d327a7889
JaxterMG <eugeny.craevsky@gmail.com>
2024-08-08 12:34:53 +0300

    Window config

---
+++ Runtime/Windows/Configs/WindowConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI.Windows.Configs
+{
+	[CreateAssetMenu(fileName = "WindowConfig", menuName = "Configs/WindowConfig", order = 1)]
+	public class WindowConfig : ScriptableObject
+	{
+		public GameObject windowPrefab;
+		public bool IsDisposable;
+		public DoTweenAnimationConfig showAnimationConfig;
+		public DoTweenAnimationConfig hideAnimationConfig;
+	}
+}
